00fe5609a7eef916deeb9d01e7de6afab9e02a97
KolanPiotr <148089653+KolanPiotr@users.noreply.github.com>
2026-05-18 18:15:44 +0200

    Implement VSS package logic (#3)

---
+++ distributed-key-lab/src/dkglab/secret_sharing/__init__.py
@@ -0,0 +1,6 @@
+"""Secret sharing primitives and recovery logic."""
+
+from dkglab.secret_sharing.recovery import recover_secret
+from dkglab.secret_sharing.splitting import create_shares
+
+__all__ = ["recover_secret", "create_shares"]
